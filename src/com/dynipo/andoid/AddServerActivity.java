1749ec3bf853042a02e25c97b9947242fccadbed
Luke Yeager <luke.yeager@gmail.com>
2014-07-17 15:35:54 -0500

    Added AddServerActivity
    Doesn't do anything yet

--- src/com/dynipo/andoid/AddServerActivity.java
+++ src/com/dynipo/andoid/AddServerActivity.java
@@ -1,23 +1,24 @@
 package com.dynipo.andoid;
 
-import android.content.Intent;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class ServerListActivity extends ActionBarActivity {
+public class AddServerActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_server_list);
+		setContentView(R.layout.activity_add_server);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.server_list, menu);
+		getMenuInflater().inflate(R.menu.add_server, menu);
 		return true;
 	}
 
@@ -26,12 +27,11 @@ public class ServerListActivity extends ActionBarActivity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_add) {
-			Intent intent = new Intent(this, AddServerActivity.class);
-		    startActivity(intent);
-			return true;
-		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	/** Called when the user clicks the Add button */
+	public void sendMessage(View view) {
+		// TODO
 	}
 }
